a9670d5df0ce9dab4b2b185956ac5f43d74aa6af
Casper da Costa-Luis <casper.dcl@physics.org>
2022-12-10 19:53:41 +0000

    day1: minify

--- aoc/sol2022/__init__.py
+++ aoc/sol2022/__init__.py
@@ -11,9 +11,7 @@ def day1():
     """Reducing & sorting lists."""
     x = open("1.txt").read().strip().split("\n\n")
     x = nlargest(3, (sum(map(int, i.split())) for i in x))
-    res1 = x[0]
-    res2 = sum(x)
-    return res1, res2
+    return x[0], sum(x)
 
 
 def day2():
